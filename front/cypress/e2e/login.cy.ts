b870261a78f504ba1360738bc6f26ed11773cc5c
Jean-Baptiste Poujol <47899256+jbpoujol@users.noreply.github.com>
2024-03-29 10:04:15 +0100

    test: add login e2e cypress test

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,30 +1,31 @@
-describe('Login spec', () => {
-  it('Login successfull', () => {
+describe('Login Component', () => {
+  beforeEach(() => {
     cy.visit('/login');
+  });
 
-    cy.intercept('POST', '/api/auth/login', {
-      body: {
-        id: 1,
-        username: 'userName',
-        firstName: 'firstName',
-        lastName: 'lastName',
-        admin: true,
-      },
-    });
+  it('Should display an error message on wrong login/password', () => {
+    cy.get('[formControlName="email"]').type('email@example.com');
+    cy.get('[formControlName="password"]').type('wrongpassword');
+    cy.get('form').submit();
 
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/api/session',
-      },
-      []
-    ).as('session');
+    cy.get('.error').should('be.visible');
+  });
 
-    cy.get('input[formControlName=email]').type('yoga@studio.com');
-    cy.get('input[formControlName=password]').type(
-      `${'test!1234'}{enter}{enter}`
-    );
+  it('Should display an error message on missing required field', () => {
+    cy.get('[formControlName="email"]').type('wrong@email.com');
+    cy.get('[formControlName="password"]').type('wrongPassword');
+    cy.get('form').submit();
 
+    cy.get('.error').should('be.visible');
+  });
+
+  it('Should redirect to sessions page after successful login', () => {
+    // Ensure simulating a successful login based on your backend
+    cy.get('[formControlName="email"]').type('yoga@studio.com');
+    cy.get('[formControlName="password"]').type('test!1234');
+    cy.get('form').submit();
+
+    // Wait for redirection to sessions page
     cy.url().should('include', '/sessions');
   });
 });
