7244161eafccb9216ada80fa2e4a223740b87bd0
Jean-Baptiste Poujol <47899256+jbpoujol@users.noreply.github.com>
2024-04-02 11:11:52 +0200

    test: refactoring

--- front/cypress/e2e/session.cy.ts
+++ front/cypress/e2e/session.cy.ts
@@ -1,5 +1,5 @@
 describe('Session spec as admin', () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit('/login');
     cy.get('[formControlName="email"]').type('yoga@studio.com');
     cy.get('[formControlName="password"]').type('test!1234');
@@ -12,30 +12,27 @@ describe('Session spec as admin', () => {
   });
 
   it('Should display "Create" button for admin user', () => {
-    cy.wait(1000);
     cy.get('button').contains('Create').should('be.visible');
   });
 
   it('Should display "Detail" button for admin user', () => {
-    cy.wait(1000);
     cy.get('button').contains('Detail').should('be.visible');
   });
 
   it('Should navigate to create page when "Create" button is clicked', () => {
-    cy.wait(1000);
     cy.get('button').contains('Create').click();
     cy.url().should('include', '/create');
+    cy.contains('mat-icon', 'arrow_back').click();
   });
 
   it('Should navigate to detail page when "Detail" button is clicked', () => {
-    cy.wait(1000);
     cy.get('button').contains('Detail').first().click();
     cy.url().should('include', '/detail');
   });
 });
 
 describe('Session spec as non admin', () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit('/login');
     cy.get('[formControlName="email"]').type('john.doe@example.com');
     cy.get('[formControlName="password"]').type('password123');
@@ -48,17 +45,14 @@ describe('Session spec as non admin', () => {
   });
 
   it('Should not display "Create" button for regular user', () => {
-    cy.wait(1000);
     cy.get('button').contains('Create').should('not.exist');
   });
 
   it('Should display "Detail" button for regular user', () => {
-    cy.wait(1000);
     cy.get('button').contains('Detail').should('be.visible');
   });
 
   it('Should navigate to detail page when "Detail" button is clicked', () => {
-    cy.wait(1000);
     cy.get('button').contains('Detail').first().click();
     cy.url().should('include', '/detail');
   });
