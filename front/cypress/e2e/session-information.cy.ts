99a42f8e00754c9e7ca740b149549282eb760d90
Jean-Baptiste Poujol <47899256+jbpoujol@users.noreply.github.com>
2024-04-02 11:03:20 +0200

    test: refactoring

--- front/cypress/e2e/session-information.cy.ts
+++ front/cypress/e2e/session-information.cy.ts
@@ -1,5 +1,5 @@
 describe('Session information spec as admin', () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit('/login');
     cy.get('[formControlName="email"]').type('yoga@studio.com');
     cy.get('[formControlName="password"]').type('test!1234');
@@ -17,16 +17,12 @@ describe('Session information spec as admin', () => {
   });
 
   it('Should display "Delete" button for admin user', () => {
-    cy.wait(1000);
-    cy.get('button').contains('Detail').first().click();
-    cy.url().should('include', '/detail');
-    cy.wait(1000);
     cy.get('button').contains('Delete').should('be.visible');
   });
 });
 
 describe('Session information spec as non-admin', () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit('/login');
     cy.get('[formControlName="email"]').type('john.doe@example.com');
     cy.get('[formControlName="password"]').type('password123');
@@ -44,10 +40,6 @@ describe('Session information spec as non-admin', () => {
   });
 
   it('Should not display "Delete" button for non-admin user', () => {
-    cy.wait(1000);
-    cy.get('button').contains('Detail').first().click();
-    cy.url().should('include', '/detail');
-    cy.wait(1000);
     cy.get('button').contains('Delete').should('not.exist');
   });
 });
